68c09180ff53afbf6efa3350b0077a992402bd2e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-25 14:20:44 +0900

    Initial commit - Next portfolio project

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true, // React Strict Mode 활성화
+  swcMinify: true, // SWC를 사용하여 빌드 최적화
+};
+
+export default nextConfig;
