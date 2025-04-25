86af195b44265f9882b97ad5145ce6d9905f5c7d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-25 17:48:20 +0900

    fix:swc-제거 - 현재 버전 즉 최신 버전 Next에서는 빌드에 자동적으로 사용된다고 함 - 해당 코드로 인하여 빌드 로그에 오류 발생

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true, // React Strict Mode 활성화
-  swcMinify: true, // SWC를 사용하여 빌드 최적화
 };
 
 export default nextConfig;
