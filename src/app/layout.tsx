5d4ae44e49ac7f929f3c5e15c93eec04708e3d7a
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-25 16:40:04 +0900

    feat:메타데이터 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
-import type { Metadata } from "next";
 import "../../styles/globals.css";
 import Link from "next/link";
-
-export const metadata: Metadata = {
-  title: "WJG - Portfolio",
-  description: "UX를 중시하는 프론트엔드 개발자 우준규입니다.",
-};
+import { DefaultSeo } from "next-seo";
 
 export default function RootLayout({
   children,
@@ -14,20 +9,58 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <header>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-        </ul>
-      </header>
+      <head>
+        <DefaultSeo
+          titleTemplate="%s | WJG 포트폴리오"
+          defaultTitle="WJG - 프론트엔드 개발자 포트폴리오"
+          description="UX를 중시하는 프론트엔드 개발자 우준규입니다."
+          canonical="https://wjg-my-portfolio.vercel.app/"
+          openGraph={{
+            type: "website",
+            locale: "ko_KR",
+            url: "https://wjg-my-portfolio.vercel.app/",
+            siteName: "WJG 포트폴리오",
+            title: "WJG - 프론트엔드 개발자 포트폴리오",
+            description: "UX를 중시하는 프론트엔드 개발자 우준규입니다.",
+            // images: [
+            //   {
+            //     url: "https://wjg-portfolio.vercel.app/og-image.jpg",
+            //     width: 1200,
+            //     height: 630,
+            //     alt: "WJG 포트폴리오",
+            //   },
+            // ],
+          }}
+          additionalMetaTags={[
+            {
+              name: "keywords",
+              content: "프론트엔드, 개발자, 포트폴리오, React, Next.js, UX",
+            },
+            {
+              name: "author",
+              content: "우준규",
+            },
+          ]}
+        />
+      </head>
       <body>
-        {children}
+        <header>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </header>
+        <main>{children}</main>
+        <footer>
+          <div>
+            <h1>WJG</h1>
+          </div>
+        </footer>
       </body>
-      <footer>
-        <div>
-          <h1>WJG</h1>
-        </div>
-      </footer>
     </html>
   );
 }
