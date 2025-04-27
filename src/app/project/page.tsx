d61c66ecde5a73bf37f19610e0559689e41842b0
WJG <dnwnsrb11@naver.com>
2025-04-27 19:56:04 +0900

    feat:프로젝트 페이지 추가

---
+++ src/app/project/page.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  const ogImageUrl = "/img/testPortfolio.png"; // 루트 상대 경로 문자열 사용
+  return (
+    <section>
+      <div>
+        <h1>Project</h1>
+        <h1>제 이름은 우준규</h1>
+        <img src={ogImageUrl} alt="testImg" />
+      </div>
+    </section>
+  );
+};
+
+export default About;
