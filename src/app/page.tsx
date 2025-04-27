2ded702a31c76702d0a823c9c38bf17fab117e60
WJG <dnwnsrb11@naver.com>
2025-04-27 20:46:08 +0900

    design:색상 변경 텍스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1>문제 해결의 길을 걷는</h1>
       <h1>
-        <span>FrontEnd 개발자</span>입니다
+        <span className="text-title">FrontEnd 개발자</span>입니다
       </h1>
     </div>
   );
