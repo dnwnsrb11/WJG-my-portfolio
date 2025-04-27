bd5c0bde37ce63c1c41bda92791ce454ab6e6e2c
WJG <dnwnsrb11@naver.com>
2025-04-27 20:59:54 +0900

    feat:test-테일윈드

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.{js,ts,jsx,tsx}", // 프로젝트의 모든 JS/TS 파일 포함
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // app 디렉토리 추가
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  safelist: ["text-deactivate", "hover:text-title", "text-title", "bg-white", "bg-background", "text-primary"],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
