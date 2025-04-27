dae5823d44c1624666bf053125c42b641742426d
WJG <dnwnsrb11@naver.com>
2025-04-27 14:28:40 +0900

    feat:not-found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+// app/not-found.tsx 또는 app/not-found.js 파일 생성
+"use client";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>페이지를 찾을 수 없습니다</h2>
+      <p>요청하신 페이지가 존재하지 않습니다.</p>
+    </div>
+  );
+}
