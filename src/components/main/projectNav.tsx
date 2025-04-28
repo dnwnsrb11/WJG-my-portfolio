40f13aff9462454d33be7b2af83af5510d61b18c
WJG <dnwnsrb11@naver.com>
2025-04-28 20:57:28 +0900

    design:gap 값 변경

--- src/components/main/projectNav.tsx
+++ src/components/main/projectNav.tsx
@@ -3,7 +3,7 @@ export const ProjectNav = () => {
   const Git = "/img/github.svg";
   const Notion = "/img/notion.svg";
   return (
-    <section className="bg-background py-3 px-14 rounded-2xl mt-4 flex gap-40">
+    <section className="bg-background py-3 px-14 rounded-2xl mt-4 flex gap-28">
       <a href="https://velog.io/@dnwnsrb11/posts" target="_blank" rel="noopener noreferrer" className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
         <div className="w-10 h-10 rounded-md overflow-hidden">
           <img src={Velog} alt="VelogImg" />
