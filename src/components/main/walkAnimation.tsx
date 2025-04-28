8d27ca3a0ccfda5e06fd5b37a860ab3cafa21dcc
WJG <dnwnsrb11@naver.com>
2025-04-28 21:53:44 +0900

    feat:로티 애니메이션 컴포넌트 생성

--- src/components/main/walkAnimation.tsx
+++ src/components/main/walkAnimation.tsx
@@ -1,3 +1,17 @@
-export const WalkAnimation = () => {
-  return <div></div>;
+"use client";
+
+import Lottie from "lottie-react";
+import walkAnimation from "@/assets/lottie/walkAnimation.json";
+
+interface WalkAnimationProps {
+  width: number;
+  height: number;
+}
+
+export const WalkAnimation = ({ width, height }: WalkAnimationProps) => {
+  return (
+    <div style={{ width, height }}>
+      <Lottie animationData={walkAnimation} loop={true} />
+    </div>
+  );
 };
