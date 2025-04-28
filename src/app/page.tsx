197a00ad62a292065b820c4bc67bdd64a8b40001
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-28 12:28:15 +0900

    desing:기본 메인 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 export default function Home() {
   return (
-    <div>
-      <h1>문제 해결의 길을 걷는</h1>
-      <h1>
-        <span className="text-title">FrontEnd 개발자</span>입니다
-      </h1>
-    </div>
+    <section className="flex flex-col items-center pt-20">
+      <article>
+        <div className="text-7xl flex flex-col gap-2 items-center">
+          <h1>문제 해결의 길을 걷는</h1>
+          <h1>
+            <span className="text-title font-bold">FrontEnd 개발자</span>입니다
+          </h1>
+        </div>
+        <div className="mt-10 text-center">
+          <p className="text-xl font-light">
+            자세한 저의 <span className="font-bold text-title">프로젝트</span> 확인이 가능합니다.
+          </p>
+        </div>
+      </article>
+      <article className="bg-background py-5 px-16 rounded-2xl mt-4 flex gap-40">
+        <button>
+          <p>Velog-JUN.log</p>
+        </button>
+        <button>
+          <p>Git</p>
+        </button>
+        <button>
+          <p>Notion</p>
+        </button>
+      </article>
+    </section>
   );
 }
