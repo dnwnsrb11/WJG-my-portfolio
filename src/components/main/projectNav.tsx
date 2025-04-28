b29edc3fa42e237deb5e847e26d7b701a0d4365e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-28 12:56:29 +0900

    feat:버튼 -> a태그로 사이트 연결

--- src/components/main/projectNav.tsx
+++ src/components/main/projectNav.tsx
@@ -4,24 +4,29 @@ export const ProjectNav = () => {
   const Notion = "/img/notion.svg";
   return (
     <section className="bg-background py-5 px-14 rounded-2xl mt-4 flex gap-40">
-      <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
+      <a href="https://velog.io/@dnwnsrb11/posts" target="_blank" rel="noopener noreferrer" className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
         <div className="w-10 h-10 rounded-md overflow-hidden">
           <img src={Velog} alt="VelogImg" />
         </div>
         <p className="text-lg">Velog</p>
-      </button>
-      <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
+      </a>
+      <a href="https://github.com/dnwnsrb11" target="_blank" rel="noopener noreferrer" className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
         <div className="w-10 h-10 rounded-md overflow-hidden">
           <img src={Git} alt="VelogImg" />
         </div>
         <p className="text-lg">Git</p>
-      </button>
-      <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
+      </a>
+      <a
+        href="https://indigo-sprite-bd5.notion.site/1b14ba04719a80378063ccf6b7f2f448?pvs=74"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300"
+      >
         <div className="w-10 h-10 rounded-md overflow-hidden">
           <img src={Notion} alt="VelogImg" />
         </div>
         <p className="text-lg">Notion</p>
-      </button>
+      </a>
     </section>
   );
 };
