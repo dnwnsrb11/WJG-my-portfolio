0fc2efc788e2fdeee2339e3ca6a51304c62cb879
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-28 12:41:06 +0900

    feat:프로젝트 네브바 컴포넌트화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import { ProjectNav } from "@/components/main/ProjectNav";
+
 export default function Home() {
-  const Velog = "/img/velog.jpg";
-  const Git = "/img/github.svg";
-  const Notion = "/img/notion.svg";
   return (
     <section className="flex flex-col items-center pt-20">
       <article>
@@ -17,25 +16,8 @@ export default function Home() {
           </p>
         </div>
       </article>
-      <article className="bg-background py-5 px-14 rounded-2xl mt-4 flex gap-40">
-        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
-          <div className="w-10 h-10 rounded-md overflow-hidden">
-            <img src={Velog} alt="VelogImg" />
-          </div>
-          <p className="text-lg">Velog</p>
-        </button>
-        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
-          <div className="w-10 h-10 rounded-md overflow-hidden">
-            <img src={Git} alt="VelogImg" />
-          </div>
-          <p className="text-lg">Git</p>
-        </button>
-        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
-          <div className="w-10 h-10 rounded-md overflow-hidden">
-            <img src={Notion} alt="VelogImg" />
-          </div>
-          <p className="text-lg">Notion</p>
-        </button>
+      <article>
+        <ProjectNav />
       </article>
     </section>
   );
