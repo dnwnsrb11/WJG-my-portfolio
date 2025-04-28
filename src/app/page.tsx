07414a76093d3eefa8cca45fd2147ed5184d3bc0
WJG <dnwnsrb11@naver.com>
2025-04-28 21:01:55 +0900

    design:메인 페이지 디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,20 @@ import { ProjectNav } from "@/components/main/projectNav";
 export default function Home() {
   return (
     <section className="flex flex-col items-start">
-      <article className="flex flex-col pt-20 w-full">
-        <div className="text-7xl flex flex-col gap-2">
+      <article className="flex flex-col pt-20 w-full mx-4">
+        <div className="text-5xl flex flex-col gap-2">
           <h1>문제 해결의 길을 걷는</h1>
           <h1>
-            <span className="text-title font-bold">FrontEnd 개발자</span>입니다
+            <span className="text-title text-7xl font-bold">
+              FrontEnd 개발자
+            </span>{" "}
+            입니다
           </h1>
         </div>
       </article>
-      <article>
-        <div className="mt-10">
+      <hr className="w-full border-stroke mt-10 my-4" />
+      <article className="mx-4">
+        <div>
           <p className="text-xl font-light text-text2">
             자세한 저의 <span className="font-bold text-title">프로젝트</span>{" "}
             확인이 가능합니다.
