2fee2c50caf974b4d1a05cb08b17c8582f130f2f
WJG <dnwnsrb11@naver.com>
2025-04-28 21:09:55 +0900

    design:상단 멘트 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@ import { ProjectNav } from "@/components/main/projectNav";
 export default function Home() {
   return (
     <section className="flex flex-col items-start">
-      <article className="flex flex-col pt-20 w-full mx-4">
-        <div className="text-5xl flex flex-col gap-2">
-          <h1>문제 해결의 길을 걷는</h1>
-          <h1>
-            <span className="text-title text-7xl font-bold">
-              FrontEnd 개발자
-            </span>{" "}
-            입니다
-          </h1>
-        </div>
-      </article>
+      <section className="w-full flex gap-2 items-end">
+        <article className="flex flex-col pt-20 w-full mx-4">
+          <div className="text-5xl flex flex-col gap-2">
+            <h1>문제 해결의 길을 걷는</h1>
+            <h1>
+              <span className="text-title text-7xl font-bold">
+                FrontEnd 개발자
+              </span>{" "}
+              입니다.
+            </h1>
+          </div>
+        </article>
+        <div>lottie</div>
+      </section>
       <hr className="w-full border-stroke mt-10 my-4" />
       <article className="mx-4">
         <div>
