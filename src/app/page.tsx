ffaff15996705a2e47d1dd420ed3e494c5b6699c
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-28 12:38:46 +0900

    desing:프로젝트 네비게이션

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,19 +18,19 @@ export default function Home() {
         </div>
       </article>
       <article className="bg-background py-5 px-14 rounded-2xl mt-4 flex gap-40">
-        <button className="flex gap-4 items-center">
+        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
           <div className="w-10 h-10 rounded-md overflow-hidden">
             <img src={Velog} alt="VelogImg" />
           </div>
           <p className="text-lg">Velog</p>
         </button>
-        <button className="flex gap-4 items-center">
+        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
           <div className="w-10 h-10 rounded-md overflow-hidden">
             <img src={Git} alt="VelogImg" />
           </div>
           <p className="text-lg">Git</p>
         </button>
-        <button className="flex gap-4 items-center">
+        <button className="flex gap-4 items-center hover:text-title hover:scale-110 transition-all duration-300">
           <div className="w-10 h-10 rounded-md overflow-hidden">
             <img src={Notion} alt="VelogImg" />
           </div>
