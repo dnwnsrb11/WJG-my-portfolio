1da75cdb2b3860b4b54c78005750c30add72aa14
WJG <dnwnsrb11@naver.com>
2025-04-29 21:45:56 +0900

    design:패딩 -> aobutme 추가

--- src/components/main/aboutMe.tsx
+++ src/components/main/aboutMe.tsx
@@ -47,7 +47,7 @@ export const AboutMe = () => {
     },
   ];
   return (
-    <section className="pt-20">
+    <section className="pt-20 custom:mx-6">
       <div className="max-w-4xl mx-auto">
         <h2 className="text-4xl font-bold mb-12 text-gray-900 dark:text-white relative">
           About Me
