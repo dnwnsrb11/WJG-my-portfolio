4d46d9b26d593c519b6203ab0e99d8e0e05ea1cd
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-30 17:45:18 +0900

    feat:모션 라이브러리 설치 -> 텍스트 변화

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,14 @@
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
 const About = () => {
   const ogImageUrl = "/img/testPortfolio.png"; // 루트 상대 경로 문자열 사용
   return (
     <section>
       <div>
         <h1>About me</h1>
+        <div>
+          <TextGenerateEffect words="hello" />
+        </div>
         <h1>제 이름은 우준규</h1>
         <img src={ogImageUrl} alt="testImg" />
       </div>
