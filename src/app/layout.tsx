a1058209ca658661eddd052a6783a9e6dcfdb661
WJG <dnwnsrb11@naver.com>
2025-05-05 13:18:04 +0900

    design:최대 넓이 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,19 +34,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="flex flex-col min-h-screen relative">
+      <body className="flex flex-col min-h-screen custom:items-center relative">
         {/* 그라데이션 기능 */}
         <div className="absolute -top-40 -left-40 w-96 h-96 bg-orange-600/10 rounded-full blur-3xl"></div>
         <div className="absolute -bottom-0 -left-40 w-96 h-96 bg-orange-600/10 rounded-full blur-3xl"></div>
         <header className="fixed top-7 left-0 right-0 flex justify-center z-30">
           <NavBar />
         </header>
-        <main className="pt-24 flex-grow">{children}</main>
+        <main className="pt-24 flex-grow max-w-[1920px]">{children}</main>
         <footer className="mt-auto border-t w-full border-stroke">
           <div className="text-center">
-            <h1 className="text-[3vw] font-bold text-stroke">
-              WJG - FRONTEND DEVELOPER
-            </h1>
+            <h1 className="text-[3vw] font-bold text-stroke">WJG - FRONTEND DEVELOPER</h1>
             <div className="opacity-30">
               <p>010-2386-6916</p>
               <p>dnwnsrb11@naver.com</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <ProjectNav />
         </article>
       </section>
-      <section className="w-full mt-12">
+      <section className="w-full mt-20">
         <AboutMe />
       </section>
     </section>
