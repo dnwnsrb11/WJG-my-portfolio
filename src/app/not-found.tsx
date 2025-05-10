cd17abe67b3f1dc07ed95647b27ca8cdbd13c1f7
WJG <dnwnsrb11@naver.com>
2025-05-10 19:29:14 +0900

    design:404 페이지 스타일링

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,9 +3,17 @@
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>페이지를 찾을 수 없습니다</h2>
-      <p>요청하신 페이지가 존재하지 않습니다.</p>
-    </div>
+    <section className="flex justify-center">
+      <div
+        className="flex flex-col items-center justify-center"
+        style={{ height: "calc(100vh - 20vh)" }}
+      >
+        <h1 className="text-7xl text-title font-bold mb-5">404</h1>
+        <h2 className="text-2xl font-bold">페이지를 찾을 수 없습니다</h2>
+        <p className="text-deactivate text-xl">
+          요청하신 페이지가 존재하지 않습니다.
+        </p>
+      </div>
+    </section>
   );
 }
