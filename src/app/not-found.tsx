296b622767078a20f58c36c6b249ac1d61098d71
WJG <dnwnsrb11@naver.com>
2025-05-10 20:19:29 +0900

    feat:404페이지 홈으로 돌아기기 버튼 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import Link from "next/link";
 const NotFoundAnimation = dynamic(
   () =>
     import("@/components/notfound/not-found-animation").then((mod) => ({
@@ -12,11 +13,11 @@ const NotFoundAnimation = dynamic(
 
 export default function NotFound() {
   return (
-    <section className="flex justify-center">
-      <div
-        className="flex flex-col items-center justify-center"
-        style={{ height: "calc(100vh - 40vh)" }}
-      >
+    <section
+      className="flex flex-col justify-center items-center"
+      style={{ height: "calc(100vh - 40vh)" }}
+    >
+      <div className="flex flex-col items-center justify-center">
         <div>
           <NotFoundAnimation width={300} height={300} />
         </div>
@@ -24,6 +25,9 @@ export default function NotFound() {
         <p className="text-deactivate text-xl">
           요청하신 페이지가 존재하지 않습니다.
         </p>
+      </div>
+      <div className="p-2 border rounded-xl max-w-80 mt-4 cursor-pointer border-deactivate hover:border-title hover:text-title transition-all duration-300">
+        <Link href={"/"}>홈으로 돌아가기</Link>
       </div>
     </section>
   );
